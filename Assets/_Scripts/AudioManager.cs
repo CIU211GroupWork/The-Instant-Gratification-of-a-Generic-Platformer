1f1c4abb6cfe3d48a0789393c85b9e6e34ef1e46
Lachlan Brown <lachlan.brown8@live.com.au>
2015-04-30 18:06:54 +1000

    Basic working Project: +Character Moves/Jumps +Collectable pickups that play an PLACEHOLDER escalating sound +UI Positioned +Basic Prefabs all in

---
+++ Assets/_Scripts/AudioManager.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AudioManager : MonoBehaviour {
+
+	public AudioClip[] acPickupClips;
+	public AudioSource BGMSource;
+	public AudioSource HeartbeatSource;
+
+	public float fPickupVolume = 0.2f;
+	public float fMainMusicVolume = 1.0f;
+	public float fHeartbeatVolume = 0.0f;
+
+	private int iClipTracker = 0;
+
+	// Use this for initialization
+	void Start () {
+		BGMSource.volume = 1.0f;
+		HeartbeatSource.volume = 0.0f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void PlayNextClip() {
+		if(iClipTracker >= acPickupClips.Length) {
+			AudioSource.PlayClipAtPoint(acPickupClips[acPickupClips.Length-1], transform.position, fPickupVolume);
+		}
+		else {
+			fPickupVolume += 1f;
+			AudioSource.PlayClipAtPoint(acPickupClips[iClipTracker], transform.position, fPickupVolume);
+			iClipTracker++;
+
+		}
+		if(fMainMusicVolume >=0.1) {
+			fMainMusicVolume -= 0.2f;
+			BGMSource.volume = fMainMusicVolume;
+		}
+		if(fHeartbeatVolume <= 1) {
+			fHeartbeatVolume += 0.2f;
+			HeartbeatSource.volume = fHeartbeatVolume;
+		}
+	}
+}
---
+++ Assets/_Scripts/Pickup.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Pickup : MonoBehaviour {
+
+	public GameObject goFull;
+	private AudioManager audioManager;
+	private bool bIsPickedUp;
+
+	// Use this for initialization
+	void Start () {
+		audioManager = GameObject.FindWithTag("AudioManager").GetComponent<AudioManager>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter2D(Collider2D other) {
+		if(!bIsPickedUp){
+			audioManager.PlayNextClip();
+			goFull.SetActive(false);
+			bIsPickedUp = true;
+		}
+	}
+}
